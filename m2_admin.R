452236ba15cea95d928381be569b9f09a5227743
Juliette Engelaere-Lefebvre <37416795+jengelaere@users.noreply.github.com>
2023-09-15 18:00:55 +0200

    Ajout parquet et mise à jour G2R (#25)
    * ajout parquet  geoparquet (#20)
    * typo 
    * ajout de la commande pour sortir le pdf
    * Mise à jour de l'adresse de la base onyxia
    * Mise à jour des référents R (#24)

--- m2_admin.R
+++ m2_admin.R
@@ -2,3 +2,18 @@
 
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
+# Commande pour générer un support pdf du module 2
+propre.rpls::creer_pdf_book(
+  chemin_book = "_book/", nom_pdf = "M2_daprep.pdf",
+  pages_html = c(
+    "index.html",
+    "le-tidyverse.html",
+    "bien-commencer.html",
+    "lire-des-données.html",
+    "manipuler-des-données.html",
+    "manipuler-plusieurs-tables.html",
+    "structurer-ses-tables.html",
+    "nettoyer-des-données-territoriales-gérer-les-évolutions-du-cog-code-officiel-géographique.html",
+    "exercice-6-les-données-majic.html",
+    "aller-plus-loin.html"
+  ))
